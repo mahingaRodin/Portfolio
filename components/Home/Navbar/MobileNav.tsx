5302b065ef8d8c8f12d7b4fff320b886a974c0e5
P-Odiix <mahingarodin@gmail.com>
2025-01-15 21:22:55 +0200

    "updated "

--- components/Home/Navbar/MobileNav.tsx
+++ components/Home/Navbar/MobileNav.tsx
@@ -1,8 +1,26 @@
+import { navLinks } from "@/constant/constant"
+import Link from "next/link"
 import React from 'react'
 
 const MobileNav = () => {
   return (
-    <div>MobileNav</div>
+      <div>
+          {/* Overlay */}
+          <div className="fixed inset-0 z-[1000] bg-black opacity-70 w-full h-screen"></div>
+          {/* Nav links */}
+          <div className="text-white fixed justify-center flex flex-col h-full w-[80%] sm:w-[60%] sm:w-[60%] bg-[#0f0175]
+          space-x-6 z-[10000]">
+              {navLinks.map((navlink) => {
+                  return (
+                      <Link key={navlink.id} href={navlink.url}>
+                          <p className="nav__link text-[20px] ml-12 border-b-[1.5px] pb-2 border-white sm:text-[30px]">
+                              {navlink.label}
+                          </p>
+                      </Link>
+                  )
+              })}
+        </div>
+    </div>
   )
 }
 
--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Image from "next/image";
 import { navLinks } from "@/constant/constant";
 import Link from "next/link";
+import { HiBars3BottomRight } from "react-icons/hi2";
 
 const Nav = () => {
   return (
@@ -31,11 +32,13 @@ const Nav = () => {
           {/* Button */}
           <div className="flex items-center space-x-4">
             <button
-              className="md:px-10 md:py-3 px-8 py-3 text-blue-800 font-semibold sm:text-base text-sm bg-white
+              className="md:px-10 md:py-3 px-8 py-3 text-black font-semibold sm:text-base text-sm bg-white
                       hover:bg-gray-200 transition-all duration-200 rounded-lg"
             >
               Hire me
             </button>
+            {/* Burger */}
+            <HiBars3BottomRight className="w-8 h-8 cursor-pointer text-white lg:hidden" />
           </div>
         </div>
       </div>
