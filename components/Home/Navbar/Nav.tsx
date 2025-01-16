7f950795dddd6f20927f415e57ee75aaaa5b3484
P-Odiix <mahingarodin@gmail.com>
2025-01-16 20:05:07 +0200

    "nav.tsx updated"

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -5,7 +5,11 @@ import { navLinks } from "@/constant/constant";
 import Link from "next/link";
 import { HiBars3BottomRight } from "react-icons/hi2";
 
-const Nav = () => {
+type Props = {
+  openNav: () => void;
+};
+
+const Nav = ({ openNav }: Props) => {
   return (
     <div className="fixed h-[12vh] z-[10] bg-blue-950 w-full">
       <div className="flex items-center h-full justify-between w-[95%] sm:w-[90%] xl:w-[80%] mx-auto">
@@ -38,7 +42,10 @@ const Nav = () => {
               Hire me
             </button>
             {/* Burger */}
-            <HiBars3BottomRight className="w-8 h-8 cursor-pointer text-white lg:hidden" />
+            <HiBars3BottomRight
+              onClick={openNav}
+              className="w-8 h-8 cursor-pointer text-white lg:hidden"
+            />
           </div>
         </div>
       </div>
