365af67dc47b8153b73da7c9e950e2657138197a
P-Odiix <mahingarodin@gmail.com>
2025-01-17 10:24:02 +0200

    Main updates

---
+++ components/Home/About/About.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+      <div className="pt-16 pb-16 bg-[#050709]">
+           {/* Section Heading */}
+    </div>
+  )
+}
+
+export default About
--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -1,5 +1,7 @@
 import { BaseInfo } from "@/Data/data";
+import { FaDownload } from "react-icons/fa";
 import React from "react";
+import Image from "next/image";
 
 const Hero = () => {
   return (
@@ -8,12 +10,33 @@ const Hero = () => {
         <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-12">
           {/* Text Content */}
           <div>
+            {/* Sub Heading */}
             <h1 className="text-2xl md:text-3xl lg:text-4xl mb-5 text-gray-300 font-semibold">
               I am {BaseInfo.name}
             </h1>
+            {/* Title */}
+            <h1 className="text-bg text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold md:leading-[3rem] lg:leading-[3.5rem] xl:leading-[4rem] text-white">
+              {BaseInfo.position}
+            </h1>
+            {/* description */}
+            <p className="mt-6 text-sm md:text-base text-white text-opacity-60">
+              {BaseInfo.description}
+            </p>
+            {/* Button */}
+            <button className="md:px-8 md:py-2.5 px-6 py-1.5 text-white font-semibold text-sm md:text-lg transition-all duration-200 rounded-lg mt-8 bg-blue-700 hover:bg-blue-900 flex items-center space-x-2 ">
+              <span>Download CV</span>
+              <FaDownload />
+            </button>
           </div>
           {/* Image Content */}
-          <div></div>
+          <div className="mx-auto hidden lg:block rounded-[3rem] border-[3.5px] border-blue-950 overflow-x-hidden">
+            <Image
+              src={BaseInfo.profilePic}
+              alt={BaseInfo.name}
+              width={300}
+              height={300}
+            />
+          </div>
         </div>
       </div>
     </div>
