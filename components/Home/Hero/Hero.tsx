e36805dec9146cb5123db8a788b8c4bbf3a30a30
P-Odiix <mahingarodin@gmail.com>
2025-01-17 08:56:38 +0200

    "added the hero.tsx"

---
+++ components/Home/Hero/Hero.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="w-full pt-[4vh] md:pt-[12vh] h-screen bg-[#0f0715] overflow-hidden relative">
+          <div className="flex justify-center flex-col w-4/5 h-full mx-auto">
+              <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-12">
+                  {/* Text Content */}
+                  <div></div>
+                  {/* Image Content */}
+                  <div></div>
+              </div>  
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
