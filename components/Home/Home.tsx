602a30193ffc19b7048d66b5d1fff1db77b636ec
P-Odiix <mahingarodin@gmail.com>
2025-01-17 15:45:17 +0200

    "updated the home"

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import Hero from "./Hero/Hero";
 import About from "./About/About";
 import Services from "./Services/Services";
+import Project from "./Project/Project";
+import Skills from "./Skills/Skills";
+import Reviews from "./Reviews/Reviews";
 
 const Home = () => {
   return (
@@ -9,9 +12,11 @@ const Home = () => {
       <Hero />
       <About />
       <Services />
+      <Project />
+      <Skills />
+      <Reviews />
     </div>
   );
-
 };
 
 export default Home;
