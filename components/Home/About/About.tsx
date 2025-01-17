0278aefc409646cc9c74f26c97bd0a74ee2c96e4
P-Odiix <mahingarodin@gmail.com>
2025-01-17 14:28:26 +0200

    "updated the about page"

--- components/Home/About/About.tsx
+++ components/Home/About/About.tsx
@@ -1,6 +1,7 @@
-import SectionHeading from "@/components/Helper/SectionHeading"
+import SectionHeading from "@/components/Helper/SectionHeading";
 import { aboutInfo } from "@/Data/data";
-import React from 'react';
+import Image from "next/image";
+import React from "react";
 import { FaCheck } from "react-icons/fa";
 
 const About = () => {
@@ -16,21 +17,108 @@ const About = () => {
           </h1>
           <p className="mt-6 text-base text-gray-500">
             {aboutInfo.description}
-                  </p>
-                  <div className="mt-8">
-                      <div className="flex items-center space-x-2 mb-6">
-                          <div className="w-7 h-7 bg-blue-800 flex flex-col items-center justify-center">
-                              <FaCheck className="text-white"/>
-                          </div>
-                          <p className="text-sm sm:text-base md:text-lg font-bold text-gray-300">
-                              Frontend Development
-                          </p>
-                      </div>
-                  </div>
+          </p>
+          <div className="mt-8">
+            <div className="flex items-center space-x-2 mb-6">
+              <div className="w-7 h-7 bg-blue-800 flex flex-col items-center justify-center">
+                <FaCheck className="text-white" />
+              </div>
+              <p className="text-sm sm:text-base md:text-lg font-bold text-gray-300">
+                Frontend Development
+              </p>
+            </div>
+          </div>
+          <div className="mt-8">
+            <div className="flex items-center space-x-2 mb-6">
+              <div className="w-7 h-7 bg-orange-800 flex flex-col items-center justify-center">
+                <FaCheck className="text-white" />
+              </div>
+              <p className="text-sm sm:text-base md:text-lg font-bold text-gray-300">
+                Backend Development
+              </p>
+            </div>
+          </div>
+          <div className="mt-8">
+            <div className="flex items-center space-x-2 mb-6">
+              <div className="w-7 h-7 bg-green-800 flex flex-col items-center justify-center">
+                <FaCheck className="text-white" />
+              </div>
+              <p className="text-sm sm:text-base md:text-lg font-bold text-gray-300">
+                Full Stack Development
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* Stats content */}
+        <div className="grid grid-cols-2 gap-16 items-center lg:mx-auto">
+          <div>
+            {/* 1st stat */}
+            <Image
+              src="/images/customer.png"
+              alt="image"
+              width={80}
+              height={80}
+              className="mx-auto"
+            />
+            <p className="mt-3 font-bold text-xl text-white text-center">
+              {aboutInfo.client}
+            </p>
+            <p className="text-base sm:text-lg text-gray-400 text-center">
+              Satisfied Customers
+            </p>
+          </div>
+          <div>
+            {/* 2nd Stat */}
+            <Image
+              src="/images/experience.png"
+              alt="image"
+              width={80}
+              height={80}
+              className="mx-auto"
+            />
+            <p className="mt-3 font-bold text-xl text-white text-center">
+              {aboutInfo.experience}
+            </p>
+            <p className="text-base sm:text-lg text-gray-400 text-center">
+              Years of Experience
+            </p>
+          </div>
+          <div>
+            {/* 3rd stat */}
+            <Image
+              src="/images/completed.png"
+              alt="image"
+              width={80}
+              height={80}
+              className="mx-auto"
+            />
+            <p className="mt-3 font-bold text-xl text-white text-center">
+              {aboutInfo.project}
+            </p>
+            <p className="text-base sm:text-lg text-gray-400 text-center">
+              Completed Projects
+            </p>
+          </div>
+          <div>
+            {/* 4th stat  */}
+            <Image
+              src="/images/rocket.png"
+              alt="image"
+              width={80}
+              height={80}
+              className="mx-auto"
+            />
+            <p className="mt-3 font-bold text-xl text-white text-center">
+              {aboutInfo.website}
+            </p>
+            <p className="text-base sm:text-lg text-gray-400 text-center">
+              Website Launched
+            </p>
+          </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default About
+export default About;
