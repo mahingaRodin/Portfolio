d825273e80b35254c1fff535e661619db0a07b4d
P-Odiix <mahingarodin@gmail.com>
2025-01-21 20:48:20 +0200

    "updated"

--- components/Home/Reviews/Slider.tsx
+++ components/Home/Reviews/Slider.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+"use client";
+import { clientReviews } from "@/Data/data";
+import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import ReviewCard from "./ReviewCard";
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1324 },
+    items: 3,
+    slidesToSlide: 1,
+  },
+  tablet: {
+    breakpoint: { max: 1324, min: 764 },
+    items: 2,
+    slidesToSlide: 1,
+  },
+  mobile: {
+    breakpoint: { max: 764, min: 0 },
+    items: 1,
+    slidesToSlide: 1,
+  },
+};
 
 const Slider = () => {
   return (
-    <div>Slider</div>
-  )
-}
+    <Carousel
+      arrows={true}
+      autoPlay={true}
+      autoPlaySpeed={5000}
+      responsive={responsive}
+      infinite
+      >
+          {clientReviews.map((review) => {
+              return (
+                  <div key={review.image}>
+                      <ReviewCard review={review} />
+                  </div>
+              )
+          })}
+    </Carousel>
+  );
+};
 
-export default Slider
+export default Slider;
