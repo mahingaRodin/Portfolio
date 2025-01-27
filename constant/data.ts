b0a349b488ec69aa4b57a202bea0920a04478936
P-Odiix <mahingarodin@gmail.com>
2025-01-27 15:57:35 +0200

    "updated"

---
+++ constant/data.ts
@@ -0,0 +1,25 @@
+export const education = [
+  {
+    id: 1,
+    title: "Primary Level",
+    summary: "Took my primary Level Education at Kigali Pacific College.",
+    date: "2014-2020",
+    image: "/images/kpc.jpg",
+  },
+  {
+    id: 2,
+    title: "Ordinary Level",
+    summary:
+      "Took my Ordinary Level Education at Groupe Scolaire Officiel de Butare where I learnt about the basic concepts of Sciences and Technology.",
+    date: "2020-2023",
+    image: "/images/gsob.jpeg",
+  },
+  {
+    id: 3,
+    title: "Advanced Level",
+    summary:
+      "Taking my Advanced Education in Rwanda Coding Academy where I am learning Software Engineering and Embedded Systems and Cyber Security.",
+    date: "Currently",
+    image: "/images/rca.png",
+  },
+];
