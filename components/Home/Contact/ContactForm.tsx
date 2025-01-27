d00452653cddb288bbef9382f3a1b93b7e47ea01
P-Odiix <mahingarodin@gmail.com>
2025-01-27 14:47:02 +0200

    "updated"

---
+++ components/Home/Contact/ContactForm.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+const ContactForm = () => {
+  return (
+    <div className="bg-[#140c1c] rounded-lg p-4 sm:p-10">
+      <h1 className="text-bg text-2xl md:text-3xl lg:text-[2.5rem] font-bold">
+        Let's work Together!
+      </h1>
+      <p className="text-gray-200 mt-3 lg:text-base text-xs md:text-sm">
+        I would like to get in contact with anyone who wants to work with me or
+        someone who wants to collaborate on some of my Projects ! It would be a
+        great pleasure!🤝
+      </p>
+      {/* Input fields */}
+      <form className="mt-8 block w-full overflow-hidden">
+        <div className="flex flex-col md:flex-row items-center justify-between gap-4">
+          <input
+            type="text"
+            placeholder="First Name"
+            className="flex-1 bg-black text-white placeholder:text-gray-600 px-6 py-3 rounded-md border-[1.5px] border-gray-200 border-opacity-15 outline-none w-full"
+          />
+          <input
+            type="text"
+            placeholder="Last Name"
+            className="flex-1 bg-black text-white placeholder:text-gray-600 px-6 py-3 rounded-md border-[1.5px] border-gray-200 border-opacity-15 outline-none w-full"
+          />
+        </div>
+        <div className="flex mt-5 flex-col md:flex-row items-center justify-between gap-4">
+          <input
+            type="text"
+            placeholder="Email Address"
+            className="flex-1 bg-black text-white placeholder:text-gray-600 px-6 py-3 rounded-md border-[1.5px] border-gray-200 border-opacity-15 outline-none w-full"
+          />
+          <input
+            type="text"
+            placeholder="Phone Number"
+            className="flex-1 bg-black text-white placeholder:text-gray-600 px-6 py-3 rounded-md border-[1.5px] border-gray-200 border-opacity-15 outline-none w-full"
+          />
+        </div>
+        <div>
+          <select
+            defaultValue=""
+            className="w-full mt-5 bg-black text-white placeholder:text-gray-600 px-4 py-3.5 rounded-md border-[1.5px] border-gray-200 border-opacity-15 outline-none"
+          >
+            <option value="" disabled>
+              Select an Option
+            </option>
+            <option value="frontend">Frontend Development</option>
+            <option value="backend">Backend Development</option>
+            <option value="fullstack">Fullstack Development</option>
+          </select>
+        </div>
+
+        <textarea
+          className="w-full mt-5 bg-black text-white placeholder:text-gray-600 px-4 py-3.5 rounded-md border-[5px] border-gray-200 border-opacity-15 outline-none"
+          rows={7}
+          placeholder="Message"
+        ></textarea>
+        <div className="">
+
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ContactForm;
