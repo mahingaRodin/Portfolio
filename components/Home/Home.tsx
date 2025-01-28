65011df2fa3d25246faf6a2455e28d353df261bc
P-Odiix <mahingarodin@gmail.com>
2025-01-28 12:11:00 +0200

    "updated"

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 import Hero from "./Hero/Hero";
 import About from "./About/About";
 import Services from "./Services/Services";
@@ -7,8 +9,24 @@ import Skills from "./Skills/Skills";
 import Reviews from "./Reviews/Reviews";
 import Blog from "./Education/Education";
 import Contact from "./Contact/Contact";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Home = () => {
+  useEffect(() => {
+    const initAOS = async () => {
+      await import("aos");
+      AOS.init({
+        duration: 1000,
+        easing: "ease",
+        once: true,
+        anchorPlacement: "top-bottom",
+      });
+    };
+
+    initAOS();
+  }, []);
+
   return (
     <div className="overflow-hidden">
       <Hero />
